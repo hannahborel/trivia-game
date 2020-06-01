dea508ac914715c95cb21679b8f53b3701ab8aad
Hannah Borel <hannahborel>
2020-06-01 10:53:11 -0500

    all basic function is working. Currently a two page application. Will coninue styling and creating multiple pages for each question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -63,11 +63,11 @@ var questions = [
 var game ={
     correct: 0,
     incorrect: 0,
-    counter: 120,
+    counter: 5,
     countdown: function(){
         game.counter--;
         $('#counter').html(game.counter);
-        if(game.couter<=0){
+        if(game.counter<=0){
             console.log("Time is up!")
             game.done();
         }
@@ -83,5 +83,93 @@ var game ={
             }
         }
         
-    }
+    },
+    done: function(){
+        $.each($('input[name="question-1"]:checked'), function(){
+            if($(this).val()==questions[0].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-2"]:checked'), function(){
+            if($(this).val()==questions[1].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-3"]:checked'), function(){
+            if($(this).val()==questions[2].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-4"]:checked'), function(){
+            if($(this).val()==questions[3].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-5"]:checked'), function(){
+            if($(this).val()==questions[4].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-6"]:checked'), function(){
+            if($(this).val()==questions[5].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-7"]:checked'), function(){
+            if($(this).val()==questions[4].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-8"]:checked'), function(){
+            if($(this).val()==questions[7].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-9"]:checked'), function(){
+            if($(this).val()==questions[8].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+        })
+        $.each($('input[name="question-10"]:checked'), function(){
+            if($(this).val()==questions[9].correctAnswer){
+                game.correctAnswer++;
+            }else{
+                game.incorrect++;
+            }
+           })
+        
+        this.result();
+        },
+
+        result: function(){
+            clearInterval(timer);
+            $('#subwrapper h2').remove();
+
+            $("#subwrapper").html('<h2> All Done!</h2');
+            $('#subwrapper').append('<h3> Correct Answers: ' + this.correct + '</h3>')
+            $('#subwrapper').append('<h3> Correct Answers: ' + this.incorrect + '</h3>')
+            $('#subwrapper').append('<h3> Unanswered: ' + (questions.length-(this.incorrect + this.correct)) + '</h3>')
+
+
+        }
+
+
 }
