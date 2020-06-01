a774b9bd31da6e350efd435ab522d557ffc70275
Hannah Borel <hannahborel>
2020-06-01 10:33:19 -0500

    Game object created to hold all of the game methods. Timer created. Game Start method completed. Moving on to 'done' method in game object.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,5 @@
 $('#start').on('click', function(){
-    $('#start').remove();
-    for(var i= 0; i < questions.length; i++){
-        $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
-        for(var ii = 0; ii < questions[i].answers.length; ii++){
-            $('#subwrapper').append("<input type= 'radio' name='question-" + i + "' value= '" + questions[i].answers[ii] + "'>" + questions[i].answers[ii])
-        }
-    }
+   game.start();
 })
 
 var questions = [
@@ -65,3 +59,29 @@ var questions = [
         correctAnswer: "Thier Religion"
     },
 ]
+
+var game ={
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function(){
+        game.counter--;
+        $('#counter').html(game.counter);
+        if(game.couter<=0){
+            console.log("Time is up!")
+            game.done();
+        }
+    },
+    start: function(){
+        timer = setInterval(game.countdown,1000)
+        $('#subwrapper').prepend("<h2> Time Remaining: <span id ='counter'> 120 </span> Seconds </h2>")
+        $('#start').remove();
+        for(var i= 0; i < questions.length; i++){
+            $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
+            for(var ii = 0; ii < questions[i].answers.length; ii++){
+                $('#subwrapper').append("<input type= 'radio' name='question-" + i + "' value= '" + questions[i].answers[ii] + "'>" + questions[i].answers[ii])
+            }
+        }
+        
+    }
+}
